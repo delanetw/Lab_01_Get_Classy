2629ad9f35526057be36f6f9b7af8718a2e909fe
Tim Delaney <delanetw@mail.uc.edu>
2024-01-29 11:52:56 -0500

    Initial

--- src/PersonFile/PersonTest.java
+++ src/PersonFile/PersonTest.java
@@ -5,7 +5,7 @@ public class PersonTest
     public static void main(String[] args)
     {
         Person person = new Person();
-        person.ID = "0001";
+        person.id = "0001";
         person.firstName = "Te";
         person.lastName = "st";
         person.title = "Mr.";
